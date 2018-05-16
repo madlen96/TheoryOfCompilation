3447a16c151c797974d73995333229921cf895da
agnieszka <agawarchol15@gmail.com>
2018-05-16 13:08:39 +0200

    add few methods to symbolTable

--- Lab2/SymbolTable.py
+++ Lab2/SymbolTable.py
@@ -2,36 +2,41 @@
 
 
 class VariableSymbol(object):
-
     def __init__(self, name, type):
         self.name = name
         self.type = type
-    #
+        #
 
 
 class SymbolTable(object):
+    def __init__(self, parent, name):  # parent scope and symbol table name
+        self.parentScope = parent
+        self.name = name
+        self.symbols = {}
 
-    def __init__(self, parent, name): # parent scope and symbol table name
-        pass
     #
 
-    def put(self, name, symbol): # put variable symbol or fundef under <name> entry
-        pass
-    #
+    def put(self, name, symbol):  # put variable symbol or fundef under <name> entry
+        if name not in self.symbols:
+            self.symbols[name] = symbol
+        else:
+            raise ValueError
 
-    def get(self, name): # get variable symbol or fundef from <name> entry
+    def get(self, name):  # get variable symbol or fundef from <name> entry
         pass
+
     #
 
     def getParentScope(self):
-        pass
+        return self.parentScope
+
     #
 
     def pushScope(self, name):
         pass
+
     #
 
     def popScope(self):
         pass
-    #
-
+        #
